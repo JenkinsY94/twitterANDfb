eea13e3b4767b1eb48760fe332cf46dedd78686c
Sun Matt <Sun Matt>
2017-02-21 18:29:10 +0800

    add origin

---
+++ flaskr/schema.sql
@@ -0,0 +1,15 @@
+drop table if exists entries;
+create table entries (
+  id integer primary key autoincrement,
+  title text not null,
+  'text' text not null
+);
+
+create table users (
+  id integer primary key autoincrement,
+  username text not null,
+  password text not null,
+  fToken text,
+  tToken text 
+);
+
